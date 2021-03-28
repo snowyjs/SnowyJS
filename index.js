2b5905de5c80f321b34c85cabdc3d1805fe5ac3e
Lucas van Bergen <80623265+snowyjs@users.noreply.github.com>
2021-03-28 10:23:52 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+function botready() {
+    console.log("✅ | The bot is ready!")
+}
+
+function math(a, b) {
+    a + b;
+}
+
+function ping() {
+    console.log("Pong!")
+}
+
+function snowyjs(){
+    console.log("✅ | Snowy-js is ready!")
+} 
+
+module.exports = { botready, math, ping, snowyjs };
